8d188e8ddf5752b71e8fdaa8d68d6cf9408bde8f
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-09-14 18:31:17 +0200

    first prototype working, needs to be factorised

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,26 @@
 let section1 = document.getElementById("section-1");
 let section2 = document.getElementById("section-2");
+let container = document.getElementById("container");
 
-section2.addEventListener("mouseover", function(e) {
-  section2.scrollIntoView({ 
+section1.addEventListener("mouseover", waitSection1, false);
+section2.addEventListener("mouseover", waitSection2, false);
+
+function waitSection1() {
+  setTimeout(scrollToSection1, 1000);
+}
+
+function waitSection2() {
+  setTimeout(scrollToSection2, 1000);
+}
+
+function scrollToSection1() {
+  document.getElementById(`section-1`).scrollIntoView({ 
     behavior: 'smooth' 
-  });
-  console.log(`mouse over`);
-}, false);
+  })
+}
 
+function scrollToSection2() {
+  document.getElementById(`section-2`).scrollIntoView({ 
+    behavior: 'smooth' 
+  })
+}
